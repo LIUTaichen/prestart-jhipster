528d13803738ffde848baa99d3060487f95fa2f9
LIUTaichen <liu_taichen@hotmail.com>
2018-07-19 11:22:27 +1200

    adding additional components

--- src/main/webapp/app/custom/custom.module.ts
+++ src/main/webapp/app/custom/custom.module.ts
@@ -7,9 +7,21 @@ import { PrestartSharedModule } from '../shared';
 import { MaterialModule } from '../shared/material.module';
 import { PlantConfirmationComponent } from './plant-confirmation/plant-confirmation.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { MeterReadingComponent } from './meter-reading/meter-reading.component';
+import { ReviewComponent } from './review/review.component';
+import { SubmissionComponent } from './submission/submission.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
     imports: [CommonModule, CustomRoutingModule, PrestartSharedModule, MaterialModule],
-    declarations: [SelectPlantComponent, PlantConfirmationComponent, QuestionsComponent]
+    declarations: [
+        SelectPlantComponent,
+        PlantConfirmationComponent,
+        QuestionsComponent,
+        MeterReadingComponent,
+        ReviewComponent,
+        SubmissionComponent,
+        ResultComponent
+    ]
 })
 export class CustomModule {}
---
+++ src/main/webapp/app/custom/meter-reading/meter-reading.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-meter-reading',
+    templateUrl: './meter-reading.component.html',
+    styleUrls: ['./meter-reading.component.css']
+})
+export class MeterReadingComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
